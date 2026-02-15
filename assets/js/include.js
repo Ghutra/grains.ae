8d12cb14686e30f8e8ee93788408b3b54e9a9633
Ghutra <156716842+Ghutra@users.noreply.github.com>
2026-02-15 13:27:48 -0800

    Create include.js

---
+++ assets/js/include.js
@@ -0,0 +1,9 @@
+document.querySelectorAll("[include-html]").forEach(async el => {
+  const file = el.getAttribute("include-html");
+  const res = await fetch(file);
+  if (res.ok) {
+    el.innerHTML = await res.text();
+  } else {
+    el.innerHTML = "Footer failed to load.";
+  }
+});
